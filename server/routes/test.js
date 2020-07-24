73bb1efe45ff2bf75e2c53b3ff59b97e42dfdf70
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2020-07-25 00:31:06 +0700

    Regenerate Letter Seeder (Part 1) 📌

--- server/routes/test.js
+++ server/routes/test.js
@@ -5,38 +5,50 @@ var fs = require('fs');
 const MongoAPI = require('../configs/mongo');
 const letterSeeder = require('../seeder/letter_categories');
 const LetterService = require('../services/letter');
+const PizZip = require('pizzip');
+const Docxtemplater = require('docxtemplater');
+var path = require('path');
 
 router.get('/', async function (req, res, next) {
-  const response = await MongoAPI.updateOne({
-    slug: 'pindah-nikah'
-  }, {
-    counter: 2
-  }, 'letter_categories');
-  res.send(response);
-  
-  return;
-  const html = fs.readFileSync('./misc/templates/test.html', 'utf-8');
-  const options = {
-    format: "A4",
-    orientation: "potrait"
-  };
-  const data = {
-    name: "Miqdad",
-    text: "lorem ipsum gannnn"
-  };
-  console.log("asd");
-  
-  const document = {
-    html,
-    data,
-    path: "./test.pdf"
+  const content = fs.readFileSync('./misc/templates/standard.docx', 'binary');
+  var zip = new PizZip(content);
+  var doc;
+  try {
+    doc = new Docxtemplater(zip);
+  } catch (e) {
+    res.send(e.message);
   }
   
-  pdf.create(document)
-    .then(res => {
-      console.log(res)
-    });
+  const data = {
+    header: 'visible',
+    letter_number: '1000 / 45.09.12.2004 / 2020',
+    name: "Miqdad Yanuar Farcha",
+    ttl: '20 Januari 1999',
+    nationality: 'Indonesia',
+    marriage_status: 'Menikah',
+    religion: 'Islam',
+    job: 'Developer',
+    nik: '35091234123423',
+    address_1: 'Perumahan BAP E-18 RT 2 RW 16',
+    address_2: 'Ambulu Jember'
+  };
   
+  doc.setData(data);
+  
+  try {
+    doc.render();
+  } catch (e) {
+    res.send(e.message);
+  }
+  
+  var buf = doc.getZip()
+    .generate({type: 'nodebuffer'});
+  
+  fs.writeFileSync(path.resolve('./misc/templates/', 'output.docx'), buf);
+  
+  res.send({
+    message: 'done'
+  });
 });
 
 router.get('/1', LetterService.getLetter);
--- server/seeder/letter_categories.js
+++ server/seeder/letter_categories.js
@@ -7,7 +7,7 @@ const FIELDS = {
     type: 'date'
   },
   nik: {
-    title: 'NIK',
+    title: 'Nomor KTP / NIK',
     name: 'nik',
     type: 'text',
     length: 16
@@ -52,17 +52,35 @@ const FIELDS = {
       'Lain-Lain'
     ]
   },
+  marriage_status: {
+    title: 'Status Pernikahan',
+    name: 'marriage_status',
+    type: 'select',
+    data: [
+      'Single',
+      'Menikah',
+      'Janda/Duda',
+      'Cerai',
+      'Lain-Lain'
+    ]
+  },
   job: {
     title: 'Pekerjaan',
     name: 'job',
     type: 'text',
     length: 32
   },
-  address: {
-    title: 'Alamat',
-    name: 'address',
+  address_1: {
+    title: 'Alamat (Baris 1)',
+    name: 'address_1',
     type: 'text',
-    length: 128
+    length: 64
+  },
+  address_2: {
+    title: 'Alamat (Baris 2)',
+    name: 'address_2',
+    type: 'text',
+    length: 64
   },
   target: {
     title: 'Tujuan',
@@ -83,18 +101,293 @@ const FIELDS = {
   }
 }
 
-async function exec(){
+async function exec() {
   const data = [
+    {
+      name: 'SKCK',
+      slug: 'skck',
+      number_format: 'asd/asd/asd',
+      counter: 1,
+      letters: [
+        {
+          name: 'Standard',
+          slug: 'standard',
+          letter_format_file: 'skck/standard.docx',
+          fields: [
+            FIELDS.date,
+            FIELDS.name,
+            FIELDS.gender,
+            FIELDS.ttl,
+            FIELDS.religion,
+            FIELDS.job,
+            FIELDS.address_1,
+            FIELDS.address_2,
+            FIELDS.etc
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Kelahiran (Akte/KK)',
+      slug: 'kelahiran',
+      number_format: 'asd/asd/asd',
+      counter: 1,
+      letters: [
+        {
+          name: 'Standard',
+          slug: 'standard',
+          letter_format_file: 'surat-kelahiran/standard.docx',
+          fields: [
+            FIELDS.date,
+            FIELDS.name,
+            FIELDS.gender,
+            FIELDS.ttl,
+            FIELDS.address_1,
+            FIELDS.address_2,
+            {
+              title: 'Anak Ke',
+              name: 'number_of_kid',
+              type: 'number',
+            },
+            {
+              title: 'Nama Ayah',
+              name: 'father_name',
+              type: 'text',
+              length: 64
+            },
+            {
+              title: 'Nama Ibu',
+              name: 'mother_name',
+              type: 'text',
+              length: 64
+            },
+            FIELDS.etc
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Kematian',
+      slug: 'kematian',
+      number_format: 'asd/asd/asd',
+      counter: 1,
+      letters: [
+        {
+          name: 'Standard',
+          slug: 'standrad',
+          letter_format_file: 'surat-kematian/standard.docx',
+          fields: [
+            FIELDS.date,
+            FIELDS.nik,
+            FIELDS.no_kk,
+            FIELDS.name,
+            FIELDS.gender,
+            FIELDS.age,
+            FIELDS.address_1,
+            FIELDS.address_2,
+            {
+              title: 'Tanggal Kematian',
+              name: 'death_date',
+              type: 'date'
+            },
+            {
+              title: 'Tempat Kematian',
+              name: 'death_place',
+              type: 'text'
+            },
+            FIELDS.etc
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Lain Lain',
+      slug: 'lain-lain',
+      number_format: 'asd/asd/asd',
+      counter: 1,
+      letters: [
+        {
+          name: 'Surat Domisili',
+          slug: 'domisili',
+          letter_format_file: 'lain-lain/domisili.docx',
+          fields: [
+            FIELDS.name,
+            FIELDS.gender,
+            FIELDS.ttl,
+            FIELDS.religion,
+            FIELDS.job,
+            FIELDS.marriage_status,
+            FIELDS.nik,
+            FIELDS.address_1,
+            FIELDS.address_2,
+            FIELDS.etc
+          ]
+        },
+        {
+          name: 'Surat Keterangan Tidak Mampu (Mahasiswa)',
+          slug: 'sktm-mahasiswa',
+          letter_format_file: 'lain-lain/sktm-mahasiswa.docx',
+          fields: [
+            FIELDS.name,
+            FIELDS.gender,
+            FIELDS.ttl,
+            {
+              title: 'Kampus',
+              name: 'school',
+              type: 'text',
+              length: 64
+            },
+            FIELDS.religion,
+            FIELDS.address_1,
+            FIELDS.address_2,
+            {
+              title: 'Nama Wali',
+              name: 'parent_name',
+              type: 'text',
+              length: 64
+            },
+            {
+              title: 'Jenis Kelamin Wali',
+              name: 'parent_gender',
+              type: 'select',
+              data: [
+                'Laki-Laki',
+                'Perempuan'
+              ]
+            },
+            {
+              title: 'TTL Wali',
+              name: 'parent_ttl',
+              type: 'text',
+              length: 32
+            },
+            {
+              title: 'Pekerjaan Wali',
+              name: 'parent_job',
+              type: 'text',
+              length: 64
+            },
+            {
+              title: 'Agama Wali',
+              name: 'parent_religion',
+              type: 'select',
+              data: [
+                'Islam',
+                'Kristen',
+                'Khatolik',
+                'Budha',
+                'Hindu',
+                'Lain-Lain'
+              ]
+            },
+            {
+              title: 'Alamat Wali (Baris 1)',
+              name: 'parent_address_1',
+              type: 'text',
+              length: 64
+            },
+            {
+              title: 'Alamat Wali (Baris 2)',
+              name: 'parent_address_2',
+              type: 'text',
+              length: 64
+            },
+            FIELDS.etc
+          ]
+        },
+        {
+          name: 'Surat Keterangan Tidak Mampu (Siswa)',
+          slug: 'sktm-siswa',
+          letter_format_file: 'lain-lain/sktm-siswa.docx',
+          fields: [
+            FIELDS.name,
+            FIELDS.gender,
+            FIELDS.ttl,
+            {
+              title: 'Sekolah',
+              name: 'school',
+              type: 'text',
+              length: 64
+            },
+            FIELDS.religion,
+            FIELDS.address_1,
+            FIELDS.address_2,
+            {
+              title: 'Nama Wali',
+              name: 'parent_name',
+              type: 'text',
+              length: 64
+            },
+            {
+              title: 'Jenis Kelamin Wali',
+              name: 'parent_gender',
+              type: 'select',
+              data: [
+                'Laki-Laki',
+                'Perempuan'
+              ]
+            },
+            {
+              title: 'TTL Wali',
+              name: 'parent_ttl',
+              type: 'text',
+              length: 32
+            },
+            {
+              title: 'Pekerjaan Wali',
+              name: 'parent_job',
+              type: 'text',
+              length: 64
+            },
+            {
+              title: 'Agama Wali',
+              name: 'parent_religion',
+              type: 'select',
+              data: [
+                'Islam',
+                'Kristen',
+                'Khatolik',
+                'Budha',
+                'Hindu',
+                'Lain-Lain'
+              ]
+            },
+            {
+              title: 'Alamat Wali (Baris 1)',
+              name: 'parent_address_1',
+              type: 'text',
+              length: 64
+            },
+            {
+              title: 'Alamat Wali (Baris 2)',
+              name: 'parent_address_2',
+              type: 'text',
+              length: 64
+            },
+            FIELDS.etc
+          ]
+        }
+      ]
+    },
+    
+    
     {
       name: 'Pindah Nikah',
       slug: 'pindah-nikah',
       number_format: 'asd/asd/asd',
-      letter_format_file: 'pindah-nikah',
       counter: 1,
-      fields: [
-        FIELDS.name,
-        FIELDS.address,
-        FIELDS.target
+      letters: [
+        {
+          name: 'Pindah Nikah',
+          slug: 'pindah-nikah',
+          letter_format_file: 'pindah-nikah',
+          fields: [
+            FIELDS.name,
+            FIELDS.address_1,
+            FIELDS.address_2,
+            FIELDS.target
+          ]
+        }
       ]
     },
     {
@@ -112,73 +405,9 @@ async function exec(){
         FIELDS.ttl,
         FIELDS.religion,
         FIELDS.job,
-        FIELDS.address,
+        FIELDS.address_1,
+        FIELDS.address_2,
         FIELDS.target
-      ]
-    },
-    {
-      name: 'SKCK',
-      slug: 'skck',
-      number_format: 'asd/asd/asd',
-      letter_format_file: 'skck',
-      counter: 1,
-      fields: [
-        FIELDS.date,
-        FIELDS.name,
-        FIELDS.gender,
-        FIELDS.ttl,
-        FIELDS.religion,
-        FIELDS.job,
-        FIELDS.address,
-        FIELDS.etc
-      ]
-    },
-    {
-      name: 'Kelahiran (Akte/KK)',
-      slug: 'kelahiran',
-      number_format: 'asd/asd/asd',
-      letter_format_file: 'birth',
-      counter: 1,
-      fields: [
-        FIELDS.date,
-        FIELDS.name,
-        FIELDS.gender,
-        FIELDS.ttl,
-        FIELDS.address,
-        {
-          title: 'Anak Ke',
-          name: 'number-of-kid',
-          type: 'number',
-        },
-        {
-          title: 'Nama Ortu',
-          name: 'parent_name',
-          type: 'text',
-          length: 64
-        },
-        FIELDS.etc
-      ]
-    },
-    {
-      name: 'Kematian',
-      slug: 'kematian',
-      number_format: 'asd/asd/asd',
-      letter_format_file: 'death',
-      counter: 1,
-      fields: [
-        FIELDS.date,
-        FIELDS.nik,
-        FIELDS.no_kk,
-        FIELDS.name,
-        FIELDS.gender,
-        FIELDS.age,
-        FIELDS.address,
-        {
-          title: 'Tanggal Kematian',
-          name: 'death_date',
-          type: 'date'
-        },
-        FIELDS.etc
       ]
     },
     {
@@ -212,23 +441,6 @@ async function exec(){
           type: 'text',
           length: 32
         },
-        FIELDS.etc
-      ]
-    },
-    {
-      name: 'Lain Lain',
-      slug: 'lain-lain',
-      number_format: 'asd/asd/asd',
-      letter_format_file: 'other',
-      counter: 1,
-      fields: [
-        FIELDS.date,
-        FIELDS.name,
-        FIELDS.gender,
-        FIELDS.ttl,
-        FIELDS.religion,
-        FIELDS.job,
-        FIELDS.address,
         FIELDS.etc
       ]
     }
